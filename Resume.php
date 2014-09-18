b28911a0c21cdb0a31e99d7e253dcd3c26bbf0ea
jlipinio <jlipinio1>
2014-09-18 15:13:53 +0600

    Test

--- Resume.php
+++ Resume.php
@@ -56,18 +56,37 @@ class MyResume extends BanalResume implements IHopelessness
         'http://www.quizful.net/user/jlipinio' => 'Хорошие тесты, нехорошие результаты :)'
     );
 
-    private function goals_of_work($skills)
+    public $quality = array(
+        'Скромный. Тихий. Неконфликтный',
+        'Не люблю менять привычное окружение - это значит, мала вероятность, что уволюсь и уйду в др. компанию.',
+        'Без вредных привычек (Еще сюда отношу: торчание в соц.сетях)'
+    );
+
+    private function goals_of_work($skills, $in_recursion = false)
     {
         foreach($skills as $key => $value) {
             if(is_array($value))
-                $value = $this->goals_of_work($value);
+                $value = $this->goals_of_work($value, true);
             else
                 $value = 'expert';
 
             $skills[$key] = $value;
         }
 
+        if(!$in_recursion)
+            $skills[] = 'Приобрести новые знания, навыки и опыт. Влиться в коллектив.'  .
+                        'Вместе выслеживать и охотиться на баги. И вместе разделять радость от успешных релизов. :)';
+
         return $skills;
+    }
+
+    private function why_in_NGS()
+    {
+        return array(
+            'Карьерный рост: Давно существующая и известная организация с большим числом проектов. А это значит есть огромное пространство, куда расти навыкам и знаниям, и куда их потом применить.',
+            'Коллектив: Будет, у кого поучиться. Скорее всего, уже есть устоявшаяся методика управлением и разработки проектов, а мне останется только занять свое место.',
+            'Место расположения: В центре, легко добраться.'
+        );
     }
 
 } 
